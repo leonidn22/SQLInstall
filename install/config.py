b61bb2e4edba2f8e68c2f960c0a60d3bc818bfd6
leonidn22 <lneizberg@gmail.com>
2015-09-06 12:48:34 +0300

    Update config.py

--- install/config.py
+++ install/config.py
@@ -8,8 +8,8 @@ is_rerun_the_installed_version = True
 
 # default_schema will be created if not exists
 default_schema = 'optimal2'
-root = '/home/dbadmin/optinstall'
-schema_dir = '/home/dbadmin/optinstall/schema/Vertica'
+root = '/home/dbadmin/'
+schema_dir = '/home/dbadmin/schema/Vertica'
 
 log_file = path_join(root, 'logs/install.log')
 log_file = ''
